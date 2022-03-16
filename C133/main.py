0a89acbbc08526b262720e14c5bc7d1652df3ab5
vevanonarain <60396096+vevanonarain@users.noreply.github.com>
2022-03-16 17:52:02 +0530

    Add files via upload

---
+++ C133/main.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+data = pd.read_csv('Data.csv')
+list = data.iloc[:,[3,4]].values
+
+wcss = []
+for i in range(1,11):
+  kmeans = KMeans(n_clusters = i, init = 'k-means++',random_state = 42)
+  kmeans.fit(list)
+  wcss.append(kmeans.inertia_)
+
+'''plt.figure(figsize = (10,5))
+sns.lineplot(range(1,11), wcss, marker = 'o',color = 'red')
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()'''
+
+kmeans = KMeans(n_clusters=3,init = 'k-means++',random_state=42)
+y_kmeans = kmeans.fit_predict(list)
+plt.figure(figsize = (10,6))
+sns.scatterplot(list[y_kmeans == 0,0],list[y_kmeans == 0,1],color = 'yellow',label = 'Cluster 1')
+sns.scatterplot(list[y_kmeans == 1,0],list[y_kmeans == 1,1],color = 'blue',label = 'Cluster 2')
+sns.scatterplot(list[y_kmeans == 2,0],list[y_kmeans == 2,1],color = 'green',label = 'Cluster 3')
+sns.scatterplot(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],color = 'red',label = 'Centroids',s = 100,marker = ',')
+plt.title('Clustering')
+plt.xlabel('Mass')
+plt.ylabel('Radius')
+plt.legend()
+plt.show()
